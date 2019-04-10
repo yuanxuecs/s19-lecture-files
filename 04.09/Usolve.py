570a9b10a53af5380c5e9f555ee1e2fca9ab13f2
Ziad Matni <zmatni@engineering.ucsb.edu>
2019-04-10 16:39:42 -0700

    new

---
+++ 04.09/Usolve.py
@@ -0,0 +1,14 @@
+ def Usolve(U, y):
+    """Backward solve an upper triangular system Ux = y for x
+    Parameters:
+      U: the matrix, must be square, upper triangular, with nonzeros on the diagonal
+      y: the right-hand side vector
+    Output:
+      x: the solution vector to U @ x == y
+    """
+
+    print("stub - remove me before submitting")
+
+    return
+
+
