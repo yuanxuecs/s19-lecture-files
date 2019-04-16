c5d676708c0aa847dd0843f09d32fbfd96d46abc
yuanxuecs <46581699+yuanxuecs@users.noreply.github.com>
2019-04-16 01:10:50 -0700

    Update Usolve.py

--- 04.09/Usolve.py
+++ 04.09/Usolve.py
@@ -9,20 +9,19 @@ def Usolve(U, y):
       x: the solution vector to U @ x == y
     """
     # Check the input
-    m, n = U.shape
-    assert m == n, "matrix must be square"
-    assert np.all(np.triu(U) == U), "matrix U must be upper triangular"
-    assert np.all(np.diag(U) != 0), "matrix U must not be zero"
-    yn, = y.shape
-    assert yn == n, "rhs vector must be same size as U"
+    m, n = L.shape
+    assert m == n, "matrix L must be square"
+    assert np.all(np.triu(U) == U), "matrix U must be lower triangular"
+    assert np.all(np.diag(U) != 0), "matrix U must have ones on the diagonal"
     
-    # Make a copy of y that we will transform into the solution
+    # Make a copy of b that we will transform into the solution
     x = y.astype(np.float64).copy()
     
-    for col in reversed(range(n)):
-        x[col] /= U[col, col]
-        x[:col] -= x[col] * U[:col, col]
+    # Forward solve
+    for col in range(n):
+      x[:col] -= x[col] * U[:col, col]
         
     return x
+    
 
 
